98f813849685ff401a2754dad70a7f8dba934aa8
NicoNor <norieganicholas88@gmail.com>
2016-06-24 22:56:00 -0400

    Frame almsot complete.
    da

--- js/main.js
+++ js/main.js
@@ -4,10 +4,40 @@ window.onload = function(){
 /*code here*/
 $(function() {
 			$('.banner').unslider({
+        autoplay: true,
         nav: false,
         arrows: false
       });
     });
+
+
+
+
+    // show and hide sub menu
+
+	$(function(){
+
+	    $('nav ul li').hover(
+
+	        function () {
+
+	            //show its submenu
+
+	            $('ul', this).slideDown(150);
+
+	        },
+
+	        function () {
+
+	            //hide its submenu
+
+	            $('ul', this).slideUp(150);
+
+	        }
+
+	    );
+
+	});
 
   }else {
     alert("This browser does not support canvas.");
